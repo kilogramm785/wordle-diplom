68accb9929cd9395bd1c3027ad23d77a855db27f
kilogramm785 <70857718+kilogramm785@users.noreply.github.com>
2026-03-28 21:51:19 +0500

    Refactor DB connection and resource management
    Refactor database connection to use a function and ensure proper resource management with cursor and connection closure.

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ import mysql.connector
 app = Flask(__name__)
 CORS(app)
 
-db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    charset="utf8mb4"
-)
+#подключения к БД 
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        charset="utf8mb4"
+    )
 
+#логика
 def check_word(secret, guess):
     result = ["gray"] * len(guess)
     secret_list = list(secret)
@@ -32,19 +35,27 @@ def check_word(secret, guess):
 
     return result
 
+#случайное слово
 def get_random_word(length):
+    db = get_db()
     cursor = db.cursor()
+
     query = """
     SELECT word FROM words
     WHERE useful = 1 AND CHAR_LENGTH(word) = %s
     ORDER BY RAND()
     LIMIT 1
     """
+
     cursor.execute(query, (length,))
     result = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
     return result[0] if result else None
 
-# изменить потом глобальное слово
+#загаданное слово
 SECRET = get_random_word(5)
 print("SECRET:", SECRET)
 
@@ -61,19 +72,25 @@ def guess():
 
     word = data["word"]
 
+    db = get_db()
     cursor = db.cursor()
 
-    # проверка существования слова
-    cursor.execute("SELECT * FROM words WHERE word = %s", (word,))
-    if not cursor.fetchone():
-        return jsonify({"error": "Слова не существует"}), 400
+    try:
+        #проверки
+        cursor.execute("SELECT * FROM words WHERE word = %s", (word,))
+        if not cursor.fetchone():
+            return jsonify({"error": "Слова не существует"}), 400
 
-    if len(word) != len(SECRET):
-        return jsonify({"error": "Неверная длина слова"}), 400
+        if len(word) != len(SECRET):
+            return jsonify({"error": "Неверная длина слова"}), 400
 
-    result = check_word(SECRET, word)
+        result = check_word(SECRET, word)
 
-    return jsonify(result)
+        return jsonify(result)
+
+    finally:
+        cursor.close()
+        db.close()
 
 @app.route("/start", methods=["GET"])
 def start():
@@ -81,6 +98,6 @@ def start():
     SECRET = get_random_word(5)
     return jsonify({"message": "Новая игра начата"})
 
-# Запуск
+
 if __name__ == "__main__":
     app.run()
